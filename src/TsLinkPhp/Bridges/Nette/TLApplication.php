a5d9d9a8c813a5741da432463a9c944f4d548abf
Vít Peprníček <vit.peprnicek@gmail.com>
2023-11-15 09:59:35 +0100

    Nette bridge

--- src/TsLinkPhp/Bridges/Nette/TLApplication.php
+++ src/TsLinkPhp/Bridges/Nette/TLApplication.php
@@ -3,6 +3,7 @@
 namespace Murdej\TsLinkPhp\Bridges\Nette;
 
 use Murdej\TsLinkPhp\TsLink;
+use Murdej\TsLinkPhp\TsCodeGenerator;
 use Nette\Application\BadRequestException;
 use Nette\Application\Responses\FileResponse;
 use Nette\Application\Responses\TextResponse;
@@ -17,11 +18,22 @@ class TLApplication
 
     protected array $cls = [];
 
+    public string|null $codeGenFile = null;
 
+    public string $codeGenFormat = 'ts';
+
+    public string $codeGenCNRemovePrefix = '';
+
+    public string $codeGenCNRemoveSufix = 'TL';
+
+    public bool $codeGenEnabled = true;
+
+    /** @throws */
     public function run(): void
     {
         $name = substr($this->httpRequest->getUrl()->path, strlen($this->urlPrefix));
-        if (Strings::startsWith($name, 'upload/')) {
+        //todo:
+        /* if (Strings::startsWith($name, 'upload/')) {
             $fileName = substr($name, 7);
             $tf = $this->tempFileStorage->restore($fileName);
             if (!$tf) $res = [ 'status' => 'Not found' ];
@@ -30,6 +42,19 @@ class TLApplication
                 $tf->write();
             }
             (new TextResponse(json_encode($res)))->send($this->httpRequest, $this->httpResponse);
+        } else */
+        if ($name === '@code-gen' || $name === '@code-dump') {
+            if (!$this->codeGenFile && $name === '@code-gen') throw new BadRequestException('Not allowed', 405);
+            switch ($name) {
+                case '@code-gen':
+                    file_put_contents($this->codeGenFile, $this->generateClientCode());
+                    break;
+                case '@code-dump':
+                    $this->httpResponse->setContentType('text/plain', 'utf-8');
+                    $hres = new TextResponse($this->generateClientCode());
+                    $hres->send($this->httpRequest, $this->httpResponse);
+                    break;
+            }
         } else {
             if (!isset($this->cls[$name])) throw new BadRequestException("No TsLink with name '$name'", 404);
             $cl = $this->cls[$name];
@@ -52,9 +77,20 @@ class TLApplication
         public string $urlPrefix,
         protected IRequest $httpRequest,
         protected IResponse $httpResponse,
-        protected TempFileStorage $tempFileStorage,
+        //todo: protected TempFileStorage|null $tempFileStorage = null,
     )
     {
+    }
+
+    public function generateClientCode() : string
+    {
+        $tsg = new TsCodeGenerator();
+        foreach($this->getRegisterClasses() as $cn) {
+            $tsg->add($cn, $this->getLinkForClass($cn));
+        }
+        $tsg->format = $this->codeGenFormat;
+        
+        return $tsg->generateCode();
     }
 
     public function add(/*BaseCL */$cl, string |null $name = null): void
@@ -62,9 +98,8 @@ class TLApplication
         $cName = get_class($cl);
         if (!$name) {
             $name = $cName;
-            $rmPrefix = 'App\\Service\\';
-            if (Strings::startsWith($name, $rmPrefix)) $name = substr($name, strlen($rmPrefix));
-            if (Strings::endsWith($name, 'CL')) $name = substr($name, 0, -2);
+            if (Strings::startsWith($name, $this->codeGenCNRemovePrefix)) $name = substr($name, strlen($this->codeGenCNRemovePrefix));
+            if (Strings::endsWith($name, $this->codeGenCNRemoveSufix)) $name = substr($name, 0, -strlen($this->codeGenCNRemoveSufix));
             $name = Strings::webalize($name);
             $newName = $name;
             $i = 0;
